dfc38c89c61ee0b8ccd1d56da283ec853045cabd
Jet <jetarin.min@gmail.com>
2015-10-26 19:16:43 +0700

    config

--- static/app/app.js
+++ static/app/app.js
@@ -4,10 +4,10 @@ angular.module('app',
 ])
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}])
 .run(function ($cookies, $location) {
     var user_id = $cookies.get('user_id');
     if (!user_id){
         $location.path('/login');
     }
-})
+});
